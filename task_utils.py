e3f6d7bbb5a1872960c7afeec8207af3e147436c
marc <mgirona29@gmail.com>
2024-05-09 09:50:08 +0100

    add task utils

---
+++ task_utils.py
@@ -0,0 +1,30 @@
+
+
+def task_to_device(task,device='cuda'):
+    for k in task.keys():
+        task[k] = task[k].to(device)
+    return task
+
+def loaded_task(task, device='cuda'):
+    # This function is used to reshape the task to the right shape, and to load it onto the right device. 
+    if len(task['x'].shape)==4:
+        for k in task.keys():
+            if task[k].device==device:
+                task[k] = task[k].reshape(task[k].shape[1:])
+            else:
+                task[k] = task[k].reshape(task[k].shape[1:]).to(device)
+    else:
+        for k in task.keys():
+            if task[k].device==device:
+                pass
+            else:
+                task[k] = task[k].to(device)
+    return task
+
+def try_to_delete(var_list):
+    for var in var_list:
+        if var in globals():
+            print(f"{var} deleted")
+            del var
+        else:
+            print(f"{var} does not exist")
